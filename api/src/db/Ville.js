16630b129ca7ff801644adb8418a5ecc85d48758
neroark <ns_ark@hotmail.com>
2019-08-21 15:15:14 +0200

    fix: module exportssssss

--- api/src/db/Ville.js
+++ api/src/db/Ville.js
@@ -10,10 +10,38 @@ villeSchema.statics.search = search;
 
 const Ville = connection.model("Ville", villeSchema);
 
-function search() {
-  return "plein de villes";
-  /*const data = await this.find({}, { name: 1 }, { sort: { name: 1 } });
-  return data.length > 0 ? data : "no ville yet";*/
+async function search() {
+  const data = await this.find({}, { name: 1 }, { sort: { name: 1 } });
+  return data.length > 0 ? data : "no ville yet";
 }
 
-module.export = Ville;
+module.exports = Ville;
+
+/*
+const mongoose = require("mongoose");
+const db = require("../db/connection");
+
+const villesSchema = new mongoose.Schema({
+  name: String
+});
+
+villesSchema.statics.search = search;
+
+async function search(letter = "") {
+  let data;
+  if (letter) {
+    data = await this.find(
+      { name: { $regex: `^${letter}.+`, $options: "i" } },
+      { name: 1 },
+      { sort: { name: 1 } }
+    );
+    return data;
+  }
+  data = await this.find({}, { name: 1 }, { sort: { name: 1 } });
+  return data;
+}
+
+const Villes = db.model("Villes", villesSchema);
+
+module.exports = Villes;
+*/
