6b803ab6abe78faed0cd51adb32c0b3fe0580172
neroark <ns_ark@hotmail.com>
2019-08-21 22:23:03 +0200

    feat: menu base

--- front/src/App.js
+++ front/src/App.js
@@ -2,15 +2,16 @@ import React from "react";
 import List from "./containers/List";
 import "./App.css";
 import Header from "./components/Header/Header";
+import Menu from "./containers/Menu/Menu";
+import Layout from "./hoc/Layout/Layout";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <main className="main-content">
-        <h1>React Hooks</h1>
+      <Layout>
         <List />
-      </main>
+      </Layout>
     </div>
   );
 }
---
+++ front/src/hoc/Layout/Layout.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import Menu from "../../containers/Menu/Menu";
+
+export default class Layout extends Component {
+  render() {
+    return (
+      <div className="Layout">
+        <Menu />
+
+        <main className="main-content">
+          <h1>React Hooks</h1>
+          {this.props.children}
+        </main>
+      </div>
+    );
+  }
+}
