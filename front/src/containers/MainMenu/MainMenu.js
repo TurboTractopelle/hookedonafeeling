21404e1e2aabdfe15ca62cacc780cee7ef6fe78f
Guillaume Drouen <gdrouen@siege.edpsciences.net>
2019-08-23 16:04:42 +0200

    feat: add icons

--- front/src/containers/MainMenu/MainMenu.js
+++ front/src/containers/MainMenu/MainMenu.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
 import SubMenu from "../../components/SubMenu/SubMenu";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCubes } from "@fortawesome/free-solid-svg-icons";
+import { faPencilAlt } from "@fortawesome/free-solid-svg-icons";
 
 export default class MainMenu extends Component {
 	render() {
@@ -10,10 +13,12 @@ export default class MainMenu extends Component {
 					<ul>
 						<li>
 							<NavLink to="/project" exact>
-								Projects
+								<FontAwesomeIcon icon={faCubes} />
+								<span>Projects</span>
 							</NavLink>
 						</li>
 						<li>
+							<FontAwesomeIcon icon={faPencilAlt} />
 							<span className="link">Editor</span>
 						</li>
 					</ul>
